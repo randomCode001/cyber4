3a1e0a7132240a516078896e7a9e184d63befbe6
Allen Shibu <93600615+alllenshibu@users.noreply.github.com>
2023-11-27 22:04:18 +0530

    Style supabase

--- supabase-oopsie/src/App.jsx
+++ supabase-oopsie/src/App.jsx
@@ -15,28 +15,48 @@ function App() {
       .select("*")
       .eq("username", username)
       .eq("password", password);
+
+    window.alert("Invalid email or password");
   };
 
   return (
-    <>
-      <form>
-        <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          type="password"
-          id="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="button" onClick={handleSubmit}>
-          Sign Up
-        </button>
-      </form>
-    </>
+    <div class="container">
+      <div class="screen">
+        <div class="screen__content">
+          <form class="login">
+            <div class="login__field">
+              <h1 class="login__title">Log in</h1>
+              <i class="login__icon fas fa-user"></i>
+              <input
+                type="email"
+                class="login__input"
+                placeholder="Email"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div class="login__field">
+              <i class="login__icon fas fa-lock"></i>
+              <input
+                type="password"
+                class="login__input"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button class="button login__submit" onClick={handleSubmit}>
+              <span class="button__text">Log In</span>
+              <i class="button__icon fas fa-chevron-right"></i>
+            </button>
+          </form>
+        </div>
+        <div class="screen__background">
+          <span class="screen__background__shape screen__background__shape4"></span>
+          <span class="screen__background__shape screen__background__shape3"></span>
+          <span class="screen__background__shape screen__background__shape2"></span>
+          <span class="screen__background__shape screen__background__shape1"></span>
+        </div>
+      </div>
+    </div>
   );
 }
 
